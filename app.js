c8247bc407ae6ed65d37fe91d18379d3c12829f8
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-01-29 16:47:59 +0100

    I am not sure, if i want this shit

--- app.js
+++ app.js
@@ -5,24 +5,29 @@ console.log(software);
 console.log("===");
 console.log();
 
-const pm2 = require("pm2");
-pm2.connect(function(err) {
-  if (err) {
-    console.error(err);
-    process.exit(2);
-  }
-
-  pm2.list(function(err, processDescriptionList) {
+/* PM 2 Init */ {
+  // https://pm2.keymetrics.io/docs/usage/pm2-api/
+  const pm2 = require("pm2");
+  pm2.connect(function(err) {
     if (err) {
       console.error(err);
-      process.exit(1);
+      process.exit(2);
     }
-    dataToFile(processDescriptionList, "pm2_processDescription");
-    console.log(processDescriptionList);
-  });
-});
 
-function dataToFile(data, filename) {
-  const fs = require("fs");
-  fs.writeFileSync(__dirname + "/temp/" + filename + ".json", JSON.stringify(data, null, 2));
+    pm2.list(function(err, processDescriptionList) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      dataToFile(processDescriptionList, "pm2_processDescription");
+      console.log(processDescriptionList);
+    });
+  });
+}
+
+/* Helper Functions */ {
+  function dataToFile(data, filename) {
+    const fs = require("fs");
+    fs.writeFileSync(__dirname + "/temp/" + filename + ".json", JSON.stringify(data, null, 2));
+  }
 }
