23ab4aabdee17197e301824f63944a423b3186e6
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-01-29 16:36:37 +0100

    Testing PM InApp

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ var software = package_info.name + " (V " + package_info.version + ")";
 console.log(software);
 console.log("===");
 console.log();
+
+const pm2 = require("pm2");
+pm2.connect(function(err) {
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+
+  pm2.list(function(err, processDescriptionList) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    dataToFile(processDescriptionList, "pm2_processDescription");
+    console.log(processDescriptionList);
+  });
+});
+
+function dataToFile(data, filename) {
+  const fs = require("fs");
+  fs.writeFileSync(__dirname + "/temp/" + filename + ".json", JSON.stringify(data, null, 2));
+}
